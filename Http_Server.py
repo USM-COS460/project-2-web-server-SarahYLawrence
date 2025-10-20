28dbbf8edf46c641a1fadb7065e940032cf30dbc
Sarah Lawrence <sarahlawrence9927@gmail.com>
2025-10-19 20:53:54 -0400

    Fixes: - Hopfully added better comments

--- Http_Server.py
+++ Http_Server.py
@@ -18,6 +18,7 @@ def responce_codes(http_status,content,file_path):
     current_dateandtime = current_datetime()
     
     match http_status:
+        # Success Case
         case 200:
             responce_header = (
                 "HTTP/1.1 200 OK\r\n"
@@ -27,38 +28,46 @@ def responce_codes(http_status,content,file_path):
                 f"Content-Length: {len(content)}\r\n"
                 "\r\n"
             )
+        # Error Case
         case 404:
             responce_header = (
             "HTTP/1.1 404 Not Found\r\n"
             f"Date: {current_dateandtime}\r\n"
             "\r\n"
             )
+        # Unknown Case
         case _: 
             print("Unknown status")
     return responce_header
 
 def open_file(file_path):
     if os.path.isfile(file_path):
+                # Reading the file bytes
                 with open(file_path, 'rb') as f:
                     content = f.read()
     return content
 
 def parsing_file(line):
-    # document root
+    # Document root form command line
     root_header = sys.argv[1]
+    root_header = root_header.lstrip("/")
+    # Parsing file path from request command
     data = line.splitlines()[0].split(' ')
     if len(data) >= 2:
-         site_path = data[1]
-    site_path = site_path.lstrip("/")
-    root_header = root_header.lstrip("/")
+        site_path = data[1]
+        site_path = site_path.lstrip("/")
+    else:
+        site_path = ""
+    
     return site_path, root_header
 
 def respons(line):
-
     if line.startswith("GET"):
+            # Orgnizing file path
             directory_path = os.path.dirname(os.path.abspath(__file__))   
             site_path,root_header = parsing_file(line)    
             file_path = os.path.join(directory_path,root_header,site_path)
+            # Getting content from file
             if os.path.isfile(file_path):
                 content = open_file(file_path)
                 return content,responce_codes(200,content,file_path)
@@ -69,9 +78,12 @@ def respons(line):
 
 def request(conn):
     while True:
+        # Get request
         data = conn.recv(1024)
         line = data.decode('utf-8')
+        # Get responce from request
         content, response = respons(line) 
+        # Send responce
         conn.send(response.encode('utf-8'))
         if "404" in response:
             break
@@ -80,15 +92,15 @@ def request(conn):
     conn.close()
 
 def main():
-    # host and port
+    # Comandline args check 
     if len(sys.argv) != 3:
         print(f"Usage: python {sys.argv[0]} <document_root> <port>")
         sys.exit(1)
 
+    # Server Listner
     host = 'localhost'
     port = int(sys.argv[2])
 
-    # Server Listner
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((host, port))
